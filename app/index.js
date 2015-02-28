3bcc539720e980cc2e9ca563cfc55343e140e4db
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2015-02-28 17:10:08 +0530

    yeoman all

--- app/index.js
+++ app/index.js
@@ -1,12 +1,8 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-    paths: function() {
-        this.destinationRoot('desktop/yoyo');
-    },
-
     writing: function() {
-    	console.log(this.sourceRoot());
+        console.log(this.sourceRoot());
         this.fs.copyTpl(
             this.templatePath('dummyTest.js'),
             this.destinationPath('ajeeb.js')
