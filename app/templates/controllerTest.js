4bb9342f11f55605874dbdbc98440237701eeb6f
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2015-03-04 01:06:23 +0530

    comments and details

--- app/templates/controllerTest.js
+++ app/templates/controllerTest.js
@@ -20,8 +20,13 @@ describe('Controller: <%= fileName %>', function() {
         });
     }));
 
+    //If your controller is not using any http calls you can remove this test entirely . 
     it('should run the Test to get the link data from the backend', function() {
-        var controller = scope.$new(DashboardCtrl);
+
+        var controller = scope.$new( <%= componentName %> );
+
+        //this sets the expectation that , which http calls the controller gonna make , 
+        // and what response does it expects .
         httpBackend.when('GET', '/api/salesdata/dashboarddropdown')
             .respond({
                 viewLevel: 'resp.viewLevels',
@@ -30,6 +35,8 @@ describe('Controller: <%= fileName %>', function() {
                 offices: 'resp.offices'
             });
 
+        //this sets the expectation that , which http calls the controller gonna make , 
+        // and what response does it expects .
         httpBackend.expect('GET', '/api/salesdata/dashboarddropdown')
             .respond({
                 viewLevel: 'resp.viewLevels',
@@ -37,13 +44,18 @@ describe('Controller: <%= fileName %>', function() {
                 roles: 'resp.roles',
                 offices: 'resp.offices'
             });
+
         httpBackend.flush();
+
+        /* test here the the value that getting changes in the 
+         actual controller once the response received */
         expect(scope.products).toEqual('resp.products');
 
         httpBackend.verifyNoOutstandingExpectation();
         httpBackend.verifyNoOutstandingRequest();
     });
 
+    //
     it('should have a method to check the routes', function() {
         $location.path('/replaceWithRelatedUrl');
         expect($location.path()).toBe('/replaceWithRelatedUrl');
