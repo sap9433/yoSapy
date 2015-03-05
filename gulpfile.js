3f6f514e42fb55a1766cb1a596a52d1dd6ee1c61
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2015-03-06 00:21:12 +0530

    integrate cob=verage

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
+var istanbul = require('gulp-istanbul');
 
-gulp.task('test', function test() {
-    return gulp.src('test/test1.js')
-        .pipe(jasmine());
+gulp.task('test', function test(coverage) {
+    gulp.src(['app/*.js'])
+        .pipe(istanbul())
+        .on('finish', function(argument) {
+            gulp.src(['test/*.js'])
+                .pipe(jasmine())
+                .pipe(istanbul.writeReports())
+                .on('end', coverage);
+        });
 });
