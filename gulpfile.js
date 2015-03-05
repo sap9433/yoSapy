98c8d076bd36b2cc0b955d67aa7a1a587e83a3a1
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2015-03-06 00:55:09 +0530

    true assertion

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var jasmine = require('gulp-jasmine');
 var istanbul = require('gulp-istanbul');
 
 gulp.task('test', function test(coverage) {
-    gulp.src(['app/*.js'])
+    gulp.src(['app/index.js'])
         .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
         .on('finish', function(argument) {
             gulp.src(['test/*.js'])
                 .pipe(jasmine())
--- test/test1.js
+++ test/test1.js
@@ -1,5 +1,7 @@
+var assert = require('yeoman-generator').assert;
+
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+    assert.file(['gulpfile.js', 'app/index.js']);
   });
 });
